eb3fa8c7dd0373a931958758c7af091ae7ac0259
d.diaz28@ciaf.edu.co <Redredwine12345>
2018-10-12 20:10:46 -0500

    puntos parcial logica de programacion!!!

--- src/parcial_logicaa_/parcial2.java
+++ src/parcial_logicaa_/parcial2.java
@@ -15,20 +15,20 @@ public class parcial2
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) // // parcial punto 2
+    public static void main(String[] args) // // parcial punto 2 Figura
     {
-       int e;
+       int f;
        int h;     // definimos las variables
      
-     for (e=4;e>=0;e--)  // e=5, asi decidimos con este ciclo, el numero de columnas que queremos que aparezca.
+     for (f=4;f>=0;f--)  // f=4, asi decidimos con este ciclo, el numero de columnas que queremos que aparezca.
        {
-          for (h=0;h<=e;h++) // este ciclo lo ponemos, para que de las vueltas por cada columna
+          for (h=0;h<=f;h++) // este ciclo lo ponemos, para que de las vueltas por cada columna
           {
            
              System.out.print("+"); // cada vez que pasa el ciclo anterior, imprimira una cruz.
           
           }
-          System.out.println(" "); // le hago para que me espacio y haga un salto de linea
+          System.out.println(" "); // le hago para que me de un espacio y haga un salto de linea
        }
            
            
